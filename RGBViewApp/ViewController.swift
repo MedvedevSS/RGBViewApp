ecfb25fde7db9eb015ca18aae79f08e866acd57d
devGod <devgod@mail.ru>
2022-10-29 19:21:17 +0300

    added logic

--- RGBViewApp/ViewController.swift
+++ RGBViewApp/ViewController.swift
@@ -24,31 +24,28 @@ class ViewController: UIViewController {
         
         // MARK: - View
         mainView.layer.cornerRadius = 15
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
-        
-        // MARK: - Number Label
-        
         
         // MARK: - Slider
-        redSlider.value = 10
         redSlider.tintColor = .red
-        
-        greenSlider.value = 60
         greenSlider.tintColor = .green
-        
-        blueSlider.value = 200
         
     }
     @IBAction func redSliderAction(_ sender: UISlider) {
         redNumber.text = String(Int(sender.value))
+        setColor()
     }
     @IBAction func greenSliderAction(_ sender: UISlider) {
         greenNumber.text = String(Int(sender.value))
+        setColor()
     }
     @IBAction func blueSliderAction(_ sender: UISlider) {
         blueNumber.text = String(Int(sender.value))
+        setColor()
     }
     
+    private func setColor() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+    }
 }
     
     
